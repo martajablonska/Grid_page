2f70fe2603f36ffd5ef2c9a42c2358fd89c0f7a8
mjablonska <martawaldon@gmail.com>
2018-06-14 10:47:54 +0200

    Corrected style in sidebar menu

---
+++ scripts/script_menu.js
@@ -0,0 +1,17 @@
+/*const navList = document.querySelector('#nav-detailed');
+
+console.log(navList);
+
+const menuList = document.querySelector('#nav-detailed__list');
+
+const expandMenu = document.querySelector('#expand-menu');
+
+const blog = menuList.querySelector('#blog');
+
+blog.addEventListener('mouseover', function() {
+    navList.classList.add('nav-detailed--full');
+    navList.classList.remove('nav-detailed--half');
+    expandMenu.classList.add('expand-menu--none');
+    
+});*/
+
